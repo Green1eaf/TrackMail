593c438330e68c0767860d8baddd1407f36d91fd
oleg <Green1eaf@yandex.com>
2023-08-20 17:28:23 +0300

    feat: added entity for PostOffice

--- src/main/java/ru/smirnov/trackmail/model/PostOffice.java
+++ src/main/java/ru/smirnov/trackmail/model/PostOffice.java
@@ -1,7 +1,25 @@
 package ru.smirnov.trackmail.model;
 
-public class PostOffice {
-    private long id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "office")
+@SuperBuilder
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostOffice extends AbstractBaseEntity{
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "address")
     private String address;
 }
