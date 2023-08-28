b5ec50d729bb7b2a5b05179b8c57e945c45ca453
oleg <Green1eaf@yandex.com>
2023-08-28 23:19:54 +0300

    feat: added exception handler

---
+++ src/main/java/ru/smirnov/trackmail/exception/TrackMailExceptionHandler.java
@@ -0,0 +1,34 @@
+package ru.smirnov.trackmail.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+@Slf4j
+public class TrackMailExceptionHandler {
+    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    @ExceptionHandler({NotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseError notFoundHandle(Exception exception) {
+        log.error(exception.getMessage());
+        return new ResponseError(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    private static class ResponseError {
+        private final String error;
+        private final HttpStatus status;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_TIME_PATTERN)
+        private final LocalDateTime time = LocalDateTime.now();
+    }
+}
